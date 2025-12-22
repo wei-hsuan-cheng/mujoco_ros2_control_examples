530a9820301c9a3eacbb42d7f629b11a92abe922
wei-hsuan-cheng <johnathancheng0125@gmail.com>
2025-12-22 11:28:45 +0800

    fix torque error in the gravity compensation of ft_sensor

--- interactive_marker/launch/interactive_marker.launch.py
+++ interactive_marker/launch/interactive_marker.launch.py
@@ -42,7 +42,7 @@ def generate_launch_description():
     rviz_config_file = os.path.join(
         get_package_share_directory("interactive_marker"),
         "config",
-        "open_door.rviz",
+        "open_door1.rviz",
     )
 
     rviz_node = Node(
